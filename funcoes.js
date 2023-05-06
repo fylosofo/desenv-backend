9a8d6079f9dd33ab10e6c7cbdfa29824bcebb9d1
unknown <fylosofo@gmail.com>
2023-05-05 21:43:04 -0300

    add middleware update delete

--- funcoes.js
+++ funcoes.js
@@ -15,7 +15,8 @@ async function validateUsuario(req, res, pool) {
     if (req.body.cpf && req.body.senha) {
         pool.query("SELECT senha FROM usuarios where cpf=?", [req.body.cpf], async function (error, results, fields) {
             if (error) throw error;
-
+            console.log("resultsx:", results)
+            console.log("resultsxx:", results.affectedRows)
             if (results.length > 0) {// se achou usuário
                 const comparaSenha = await comparePassword(req.body.senha.toString(), results[0].senha)
                 res.send(comparaSenha ? 'Senha validada com sucesso!' : 'Senha inválida')
@@ -38,22 +39,72 @@ async function addUsuario(req, res, pool) {
         senha = await hashPassword(senha.toString())
     }
 
-    pool.query("INSERT INTO usuarios (nome, cpf, idade, email, senha) VALUES (?, ?, ?, ?, ?)",
-        [nome, cpf, idade, email, senha], function (error, results, fields) {
-            if (error) throw error;
-
-            if (results) {
-                res.send("Usuário cadastrado com sucesso!")
+    try {
+        pool.query("SELECT * FROM usuarios where cpf=?", [cpf], function (error, results, fields) {
+            if (results.length > 0) {// se achou usuário
+                res.send("Já existe usuário com este CPF!")
             }
             else {
-                res.send("Erro ao inserir usuário!")
-            }
+                pool.query("INSERT INTO usuarios (nome, cpf, idade, email, senha) VALUES (?, ?, ?, ?, ?)",
+                    [nome, cpf, idade, email, senha], function (error, results, fields) {
+                        if (error) throw error
 
+                        if (results) {
+                            res.send("Usuário cadastrado com sucesso!")
+                        }
+                        else {
+                            res.send("Erro ao inserir usuário!")
+                        }
+                    });
+            }
+        });
+    } catch (err) {
+        res.send("Erro ao inserir usuário!")
+        console.log(err)
+    }
+}
+
+async function updateUsuario(req, res, pool) {
+    let { nome, cpf, idade, email, senha } = req.body
+
+    if (senha) {
+        senha = await hashPassword(senha.toString())
+    }
+
+    pool.query("UPDATE usuarios SET nome=?, idade=?, email=?, senha=? WHERE cpf=?",
+        [nome, idade, email, senha, cpf], function (error, results, fields) {
+            if (error) throw error
+
+            if (results.affectedRows) {
+                res.send("Usuário atualizado com sucesso!")
+            }
+            else {
+                res.send("Nenhum usuário encontrado!")
+            }
+        });
+}
+
+async function deleteUsuario(req, res, pool) {
+    let { cpf } = req.body
+
+    pool.query("DELETE FROM usuarios WHERE cpf=?",
+        [cpf], function (error, results, fields) {
+            if (error) throw error;
+            console.log("resultsy:", results)
+            console.log("resultsyy:", results.affectedRows)
+            if (results.affectedRows) {
+                res.send("Usuário deletado com sucesso!")
+            }
+            else {
+                res.send("Nenhum usuário encontrado!")
+            }
         });
 }
 
 async function getUsuarioPorCpf(req, res, pool) {
-    pool.query("SELECT * FROM usuarios where cpf=?", [req.params.cpf], function (error, results, fields) {
+    let cpf = (req.body) ? req.body.cpf : req.params.cpf
+    console.log("existe", cpf)
+    pool.query("SELECT * FROM usuarios where cpf=?", [cpf], function (error, results, fields) {
         if (error) throw error;
 
         if (results.length > 0) {// se achou usuário
@@ -82,6 +133,8 @@ async function getTodosOsUsuarios(req, res, pool) {
 
 module.exports = {
     addUsuario,
+    updateUsuario,
+    deleteUsuario,
     getUsuarioPorCpf,
     getTodosOsUsuarios,
     validateUsuario
--- index.js
+++ index.js
@@ -7,32 +7,37 @@ const funcoes = require('./funcoes')
 dotenv.config()
 const app = express()
 
-app.use (bodyParser.json())
-app.use (bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const pool = mysql.createPool({
-    connectionLimit : 100, //important
-    host     : process.env.MYSQL_HOST,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PWD,
-    database : process.env.MYSQL_DB,
-    debug    :  false
+    connectionLimit: 100, //important
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
+    debug: false
 });
 
+// middleware
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+})
 
-app.listen(process.env.PORT, function() {
-    console.log('ouvindo na porta: ' +process.env.PORT)
+app.listen(process.env.PORT, function () {
+    console.log('ouvindo na porta: ' + process.env.PORT)
 });
 
-app.get('/', async function(req, res) {
+app.get('/', async function (req, res) {
     res.send('Olá Mundo!')
 });
 
-app.get('/usuarios/listar', async function(req, res) {
+app.get('/usuarios/listar', async function (req, res) {
     await funcoes.getTodosOsUsuarios(req, res, pool)
 });
 
-app.get('/usuario/cpf/:cpf', async function(req, res) {
+app.get('/usuario/cpf/:cpf', async function (req, res) {
     await funcoes.getUsuarioPorCpf(req, res, pool)
 });
 
@@ -40,7 +45,16 @@ app.post('/usuario/cadastrar', async function (req, res) {
     await funcoes.addUsuario(req, res, pool)
 });
 
+app.put('/usuario/atualizar', async function (req, res) {
+    await funcoes.updateUsuario(req, res, pool)
+});
+
+app.delete('/usuario/deletar', async function (req, res) {
+    await funcoes.deleteUsuario(req, res, pool)
+});
+
 app.post('/usuario/validar', async function (req, res) {
     await funcoes.validateUsuario(req, res, pool)
 });
+
 
